8e73b73186699004f70e72b8b5d367ab730361e3
Alex <Alex@DESKTOP-ALEX>
2023-03-15 23:24:03 +0200

    car add form

---
+++ iService3/Views/CarForm.xaml.cs
@@ -0,0 +1,9 @@
+namespace iService3.Views;
+
+public partial class CarForm : ContentPage
+{
+	public CarForm()
+	{
+		InitializeComponent();
+	}
+}
--- iService3/Views/Cars.xaml.cs
+++ iService3/Views/Cars.xaml.cs
@@ -6,4 +6,9 @@ public partial class Cars : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void addBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new CarForm());
+    }
 }
